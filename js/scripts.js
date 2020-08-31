07dde9f2b722a004b007103e4dc0cb99a7efe9c6
Dennis-Kiambi <denniskiriinya@gmail.com>
2020-08-31 11:05:49 +0300

    add functions to toggle between description and icons

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,34 @@ $(document).ready(function () {
     $('#description2').hide();
     $('#description3').hide();
 
+    $('#designing').on('click', function () {
+        $('#designing').hide();
+        $('#description1').show();
+    });
+    $('#description1').on('click', function () {
+        $('#description1').hide();
+        $('#designing').show();
+    });
+
+    $('#development').on('click', function () {
+        $('#development').hide();
+        $('#description2').show();
+    });
+    $('#description2').on('click', function () {
+        $('#description2').hide();
+        $('#development').show();
+    });
+
+    $('#product').on('click', function () {
+        $('#product').hide();
+        $('#description3').show();
+    });
+    $('#description3').on('click', function () {
+        $('#description3').hide();
+        $('#product').show();
+    });
+
+
 
 });
 
